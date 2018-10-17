bfb16ac8653560f1615ef8e90761a14fbf467520
magocoin <42104096+magocoin@users.noreply.github.com>
2018-10-16 22:18:19 -0300

    fix version

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -13,9 +13,9 @@ else
     exit 1
 fi
 
-DESC="1.1.0.0"
+DESC="1.0.0.0"
 SUFFIX="c9cf0fa"
-LAST_COMMIT_DATE="Wed Jul 18 14:59:24 2018 +0200"
+LAST_COMMIT_DATE="Wed OCT 17 14:59:24 2018 +0200"
 if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
     # clean 'dirty' status of touched files that haven't been modified
     git diff >/dev/null 2>/dev/null
